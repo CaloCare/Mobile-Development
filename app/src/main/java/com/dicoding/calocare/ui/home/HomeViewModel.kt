5b9d43fa5e200ff7dbfe5e54b913d30049b58aee
Farrel Satriano <sherrifraynor2004@gmail.com>
2024-12-06 14:12:58 +0700

    1. Menambahkan FoodAdapter.kt untuk menampilkan data food di HomeFragment 2. Merubah penamman recyclerview di fragment_home.xml 3. Menambahkan kode di HomeFragment.kt, HomeFragment.kt 4. Menambahkan parameter di ViewModelFactory.kt

--- app/src/main/java/com/dicoding/calocare/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/dicoding/calocare/ui/home/HomeViewModel.kt
@@ -3,11 +3,24 @@ package com.dicoding.calocare.ui.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.dicoding.calocare.data.remote.response.FoodItem
+import com.dicoding.calocare.data.repository.FoodRepository
+import kotlinx.coroutines.launch
+import com.dicoding.calocare.data.Result
 
-class HomeViewModel : ViewModel() {
+class HomeViewModel(private val foodRepository: FoodRepository) : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+    private val _foodList = MutableLiveData<List<FoodItem>>()
+    val foodList: LiveData<List<FoodItem>> get() = _foodList
+
+    fun getAllFood() {
+        viewModelScope.launch {
+            val result = try {
+                foodRepository.getAllFoods()
+            } catch (e: Exception) {
+                Result.Error(e.message ?: "Failed to fetch foods")
+            }
+        }
     }
-    val text: LiveData<String> = _text
 }
